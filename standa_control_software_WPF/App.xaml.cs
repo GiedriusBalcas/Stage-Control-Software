8db80dbbdfbf3700654f2e9d1c0b6193b40c0c7e
Giedrius Balčas <giedrius.balcas@gmail.com>
2024-08-05 12:35:29 +0300

    feat: added wpf project.

---
+++ standa_control_software_WPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace standa_control_software_WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
